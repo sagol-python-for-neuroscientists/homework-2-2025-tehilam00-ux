45036d917038afaed891dc0d1cfa3d4a0855aba4
תהילה מרום <thylhmrwm@MacBook-Air.local>
2026-05-17 17:47:18 +0300

    submission

--- hw2_q1.py
+++ hw2_q1.py
@@ -37,3 +37,95 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+MORSE_CODE = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    ":": "---...",
+    "'": ".----.",
+    "-": "-....-",
+}
+
+
+def english_to_morse(
+    input_file: str = "lorem.txt", output_file: str = "lorem_morse.txt"
+):
+    """Convert an input text file to an output Morse code file.
+
+    Notes
+    -----
+    This function assumes the existence of a MORSE_CODE dictionary, containing a
+    mapping between English letters and their corresponding Morse code.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to file containing the text file to convert.
+    output_file : str
+        Name of output file containing the translated Morse code. Please don't change
+        it since it's also hard-coded in the tests file.
+    """
+
+    # We store the final results here
+    translated_content = []
+
+    with open(input_file, "r") as file1:
+        # We read each line from the source
+        for line in file1:
+            line_result = []
+            for char in line.upper():
+                if char in MORSE_CODE:
+                    line_result.append(MORSE_CODE[char])
+                elif char == " " or char == "\n":
+                    # Per your requirement: spaces go to a new line
+                    line_result.append("\n")
+
+            # Join characters with a space for clarity, then add to our list
+            translated_content.append("".join(line_result))
+
+    # Join everything into one big string to write
+    final_output = "".join(translated_content)
+
+    with open(output_file, "w") as file2:
+        file2.write(final_output)
+
+    return final_output
+
+
+if __name__ == "__main__":
+    english_to_morse()
--- hw2_q2.py
+++ hw2_q2.py
@@ -1,8 +1,39 @@
-from collections import namedtuple
 from enum import Enum
+from collections import namedtuple
+from itertools import batched
 
 Condition = Enum("Condition", ("CURE", "HEALTHY", "SICK", "DYING", "DEAD"))
 Agent = namedtuple("Agent", ("name", "category"))
+
+
+def compare_agents(agent1: Agent, agent2: Agent):
+    (
+        """"" receives two agents as arguements, and return a tuple with the two agents, in their new state.
+    """
+        ""
+    )
+    if (
+        agent1[1] == Condition.DEAD
+        or agent2[1] == Condition.DEAD
+        or agent1[1] == Condition.HEALTHY
+        or agent2[1] == Condition.HEALTHY
+        or (agent1[1] == Condition.CURE and agent2[1] == Condition.CURE)
+    ):
+        return (agent1, agent2)
+    if agent1[1] == Condition.CURE:
+        i = agent2[1]
+        agent2_new = Agent(agent2[0], Condition(i.value - 1))
+        return (agent1, agent2_new)
+    if agent2[1] == Condition.CURE:
+        i = agent1[1]
+        agent1_new = Agent(agent1[0], Condition(i.value - 1))
+        return (agent1_new, agent2)
+    else:
+        i_1 = agent1[1].value
+        i_2 = agent2[1].value
+        agent1_new = Agent(agent1[0], Condition(i_1 + 1))
+        agent2_new = Agent(agent2[0], Condition(i_2 + 1))
+        return (agent1_new, agent2_new)
 
 
 def meetup(agent_listing: tuple) -> list:
@@ -19,13 +50,28 @@ def meetup(agent_listing: tuple) -> list:
     Parameters
     ----------
     agent_listing : tuple of Agent
-        A listing (tuple in this case) in which each element is of the Agent
-        type, containing a 'name' field and a 'category' field, with 'category' being
-        of the type Condition.
+        A listing (tuple in this case) in which each element is of the Agent type, containing a 'name' field and a 'category' field, with 'category' being of the type Condition.
 
     Returns
     -------
     updated_listing : list
-        A list of Agents with their 'category' field changed according to the result
-        of the meeting.
+        A list of Agents with their 'category' field changed according to the result of the meeting.
     """
+    list_of_agents = []
+    to_add = []
+    for agent in agent_listing:
+        if agent.category != Condition.HEALTHY and agent.category != Condition.DEAD:
+            list_of_agents.append(agent)
+        else:
+            to_add.append(agent)
+    list_new = list(batched(list_of_agents, n=2))
+    to_return = []
+    for tup in list_new:
+        if len(tup) == 2:
+            agent1, agent2 = compare_agents(tup[0], tup[1])
+            to_return.append(agent1)
+            to_return.append(agent2)
+        elif len(tup) == 1:
+            to_return.append(tup[0])
+    to_return += to_add
+    return to_return
